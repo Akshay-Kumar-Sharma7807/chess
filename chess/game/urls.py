138676d983be0acbdc46808757d5475120cf5d5b
Akshay Kumar Sharma <82942162+Akshay-Kumar-Sharma7807@users.noreply.github.com>
2022-11-15 10:47:58 +0000

    authentication flow made

--- chess/game/urls.py
+++ chess/game/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('play', views.play, name='play'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
+
 ]
